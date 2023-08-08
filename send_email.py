71ee3ceb540ee5cdaf50dce6646b1110f8cde73c
joethesane <joethesane@proton.me>
2023-08-08 07:42:06 -0400

    Secure password

--- send_email.py
+++ send_email.py
@@ -1,12 +1,12 @@
 import smtplib
 import ssl
-
+import os
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     username = "contact.joemad@gmail.com"
-    password = "ygunluyoioihudmg"
+    password = os.getenv("PASSWORD")
     recipient = "contact.joemad@gmail.com"
     context = ssl.create_default_context()
 
